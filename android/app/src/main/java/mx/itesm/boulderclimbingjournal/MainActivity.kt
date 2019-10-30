46314141c42989774e30755779d47059e6a7d678
carlosmartell97 <carlosmartell97@gmail.com>
2019-10-29 23:01:26 -0600

    main menu and color palette set up

--- android/app/src/main/java/mx/itesm/boulderclimbingjournal/MainActivity.kt
+++ android/app/src/main/java/mx/itesm/boulderclimbingjournal/MainActivity.kt
@@ -1,12 +1,35 @@
 package mx.itesm.boulderclimbingjournal
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val tag: String = "TAG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addSessionsButton: Button = findViewById(R.id.menu_add_session_button)
+        val sessionsLogButton: Button = findViewById(R.id.menu_sessions_log_button)
+        val statsButton: Button = findViewById(R.id.menu_stats_button)
+        val gradeConversionButton: Button = findViewById(R.id.menu_grade_conversion_button)
+
+        addSessionsButton.setOnClickListener(View.OnClickListener {
+            Log.e(tag, "add session button clicked.")
+        })
+        sessionsLogButton.setOnClickListener(View.OnClickListener {
+            Log.e(tag, "sessions log button clicked.")
+        })
+        statsButton.setOnClickListener(View.OnClickListener {
+            Log.e(tag, "stats button clicked.")
+        })
+        gradeConversionButton.setOnClickListener(View.OnClickListener {
+            Log.e(tag, "grade conversion button clicked.")
+        })
     }
 }
